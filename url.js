225f17c9af55d069e3d0d6528e33fe49b8aada98
Mahmoud Easa <mu01011422865@gmail.com>
2022-10-14 13:23:28 +0200

    Change URL

--- url.js
+++ url.js
@@ -1,3 +1,4 @@
 //
 
-export const postsURL = "http://localhost:3000/api/posts/getPosts";
+export const postsURL = "https://vercel.com/api/posts/getPosts";
+// export const postsURL = "http://localhost:3000/api/posts/getPosts";
